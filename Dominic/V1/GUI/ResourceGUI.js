1856365b63a0c2739bdd982b01186c4175679adc
Dominic <0914600@hr.nl>
2016-06-01 15:07:36 +0200

    V1 & V2

---
+++ Dominic/V1/GUI/ResourceGUI.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+
+public class ResourceGUI extends MonoBehaviour
+{
+    public var coin : Texture;
+    public var apple : Texture;
+    public var box : Texture;
+    public var font : Font;
+    
+    function OnGUI ()
+    {
+        // Make a background box
+        GUI.Box (Rect (1210,10,350,30),"" );
+
+        GUI.skin.font = font;
+
+        GUI.Label (Rect (1215,10,100,50), ResourceManager.gold + "");
+        GUI.Label (Rect (1295,10,100,50), ResourceManager.foodCurrent + " / " + ResourceManager.foodMax);
+        GUI.Label (Rect (1385,10,100,50), ResourceManager.suppliesCurrent + " / " + ResourceManager.cargoMax);
+        
+        GUI.DrawTexture(Rect(1265,13,25,25), coin);
+        GUI.DrawTexture(Rect(1357,13,25,25), apple);
+        GUI.DrawTexture(Rect(1450,13,25,25), box);
+
+
+
+        if (GUI.Button(new Rect(150, 120, 100, 30), "GAIN GOLD"))
+        {
+            ResourceManager.gold = ResourceManager.gold + 100; 
+        }
+
+        if (GUI.Button(new Rect(150, 150, 100, 30), "LOSE GOLD"))
+        {
+            ResourceManager.gold = ResourceManager.gold - 100; 
+        }
+    }
+}
---
+++ Dominic/V1/Manager/Transition.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+public class Transition extends MonoBehaviour
+{
+
+    function OnMouseDown()
+    {
+        if(this.gameObject.name == "Kroeg")
+        {
+            Application.LoadLevel ("Kroeg");
+        }
+    }
+}
